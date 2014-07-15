996adcbefdf8ede1474cdd193eee295bb45a6627
Jack Daines <dainesj@gmail.com>
2014-07-15 08:11:03 +0000

    changed filter

--- passphrase.php
+++ passphrase.php
@@ -2,11 +2,12 @@
 $WD = '/home/jakx/webapps/passphrasegen/';
 $word ='';
 for($i = 0; $i < 4; $i++){
-         while(strlen($word) > 9 || strlen($word) < 4){//|| count($word) > 4)){
+        do{
           $word =`$WD/passphrase.sh`;
         }
 //        ;
 // 	while(false);
+         while(strlen($word) -1 > 7 || strlen($word) -1 < 3);//|| count($word) > 4)){
         echo $word;
         $word ='';
 }
